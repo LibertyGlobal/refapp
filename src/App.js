07f0dbd3eec842fcbb2ed3f48b78b27f91d90c47
kathiravan <kathiravan.s@ltts.com>
2020-04-09 13:06:26 +0530

    Signed-off-by: kathiravan <kathiravan.s@ltts.com> ligthing-js-mainMenu-component-and-image-folder-added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Lightning, Utils } from 'wpe-lightning-sdk'
-import { ListItem, ImageListItem } from './core/list.js'
-import Model from './mainMenu/model.js'
-import { getBackground, getMainList, getSubMenuList } from './mainMenu/view.js'
+import Menu from './mainMenu/menu.js'
 
 export default class App extends Lightning.Component {
   static getFonts() {
@@ -10,61 +8,30 @@ export default class App extends Lightning.Component {
 
   static _template() {
     return {
-      x: 0,
-      y: 0,
-      BackGround: getBackground(),
-      MainList: getMainList(),
-      SubMenuList: getSubMenuList()
+      Menu: {
+        type: Menu,
+        alpha: 1
+      }
     }
   }
 
-  _construct() {
-    this.model = new Model()
-    this.model.data = {}
-  }
-
-  _init() {
-    this.model.getMenu().then(data => {
-      this.model.data = data
-      this.tag('MainList').ListItemsComponend = ListItem
-      this.tag('SubMenuList').ListItemsComponend = ImageListItem
-      this.tag('MainList').items = data.map(i => ({ label: i.title }))
-      this.tag('SubMenuList').items = data[0].submenu.map(i => ({ label: i }))
-      this._setState('MainList')
-    })
-  }
-
-  _handleUp() {
-    this._setState('MainList')
-  }
-
-  _handleDown() {
-    this._setState('SubMenuList')
+  _setup() {
+    this._setState('Menu')
   }
 
   static _states() {
     return [
-      class SubMenuList extends this {
+      class Menu extends this {
+        $enter() {
+          this.tag('Menu').setSmooth('alpha', 1)
+        }
+
+        $exit() {
+          this.tag('Menu').setSmooth('alpha', 0)
+        }
+
         _getFocused() {
-          return this.tag('SubMenuList')
-        }
-        select({ item }) {
-          console.log(item)
-        }
-      },
-      class MainList extends this {
-        _getFocused() {
-          return this.tag('MainList')
-        }
-        select({ item }) {
-          let data = this.model.data
-          this.tag('SubMenuList').items = []
-          for (var index in data) {
-            if (item.item.label === data[index].title) {
-              this.tag('SubMenuList').items = data[index].submenu.map(i => ({ label: i }))
-              break
-            }
-          }
+          return this.tag('Menu')
         }
       }
     ]
