91189f78f6e15200b2117bcf9da8bed7e4fcadbf
kathiravanltts <62378427+kathiravanltts@users.noreply.github.com>
2020-07-13 18:44:11 +0530

    This changes will Move to App.js
    This changes will Move to App.js .

--- buildsupport.sh
+++ buildsupport.sh
@@ -4,5 +4,3 @@ mkdir build/cache
 cp -avr ./src/img/. build/img/.
 cp -avr ./src/cache/. build/cache/.
 cp ./src/config.json build/config.json
-sed -i '31d' ./build/startApp.js
-sed -i "31 a style.sheet.insertRule('@media all { html {height: 100%; width: 100%;} *,body {margin:0; padding:0;opacity: 0.99 } canvas { position: absolute; z-index: 2; } body { width: 100%; height: 100%;} }');" ./build/startApp.js
