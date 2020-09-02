f1bf5ac03c35aafbe94ca87eac2694fd80aaa2fa
Stefan Verkoyen <stefan.verkoyen@gmail.com>
2020-09-02 09:46:13 +0200

    RDKCMF-8664 reference image: use RDKShell to start browser if supported (#33)

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -2,12 +2,21 @@
 
 function stop_browser()
 {
-  curl -X PUT http://127.0.0.1/Service/Controller/Deactivate/WebKitBrowser
+  if [ -f /etc/WPEFramework/plugins/RDKShell.json ]; then
+    curl -v --header "Content-Type:application/json" --request POST http://127.0.0.1:9998/jsonrpc --data-raw "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"org.rdk.RDKShell.1.suspend\", \"params\":{ \"callsign\": \"WebKitBrowser\"} }"
+  else
+    curl -X PUT http://127.0.0.1/Service/Controller/Deactivate/WebKitBrowser
+  fi
   exit
 }
 
-curl -X PUT http://127.0.0.1/Service/Controller/Activate/WebKitBrowser
-curl -d "{\"url\":\"$1\"}" http://127.0.0.1/Service/WebKitBrowser/URL
+if [ -f /etc/WPEFramework/plugins/RDKShell.json ]; then
+  curl -v --header "Content-Type:application/json" --request POST http://127.0.0.1:9998/jsonrpc --data-raw "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"org.rdk.RDKShell.1.launch\", \"params\":{ \"callsign\": \"WebKitBrowser\", \"uri\":\"$1\" } }"
+else
+  curl -X PUT http://127.0.0.1/Service/Controller/Activate/WebKitBrowser
+  curl -d "{\"url\":\"$1\"}" http://127.0.0.1/Service/WebKitBrowser/URL
+fi
+
 trap stop_browser SIGINT SIGTERM
 while true
 do
