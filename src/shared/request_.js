d4a64996b88944f3de4dedf7c5422efbb5986d82
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-09-14 15:28:54 +0200

    Update license information

--- src/shared/request_.js
+++ src/shared/request_.js
@@ -2,14 +2,21 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Operator reference application
+ * Copyright 2020 Liberty Global B.V.
  *
- * Copyright (C) 2018-2019 Liberty Global B.V.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2 of the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 'use strict'
 
 import http from 'http'
