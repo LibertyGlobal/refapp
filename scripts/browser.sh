7c83b9453e8bd2832711b9b731e1b35bfa3fbde4
Stefan Verkoyen <stefan.verkoyen@gmail.com>
2020-11-04 11:01:09 +0100

    RDKCMF-8698 reference image: switch to rdkservices
    which runs on port 9998

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -5,7 +5,7 @@ function stop_browser()
   if [ -f /etc/WPEFramework/plugins/RDKShell.json ]; then
     curl -v --header "Content-Type:application/json" --request POST http://127.0.0.1:9998/jsonrpc --data-raw "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"org.rdk.RDKShell.1.destroy\", \"params\":{ \"callsign\": \"WebKitBrowser\"} }"
   else
-    curl -X PUT http://127.0.0.1/Service/Controller/Deactivate/WebKitBrowser
+    curl -X PUT http://127.0.0.1:9998/Service/Controller/Deactivate/WebKitBrowser
   fi
   exit
 }
@@ -13,8 +13,8 @@ function stop_browser()
 if [ -f /etc/WPEFramework/plugins/RDKShell.json ]; then
   curl -v --header "Content-Type:application/json" --request POST http://127.0.0.1:9998/jsonrpc --data-raw "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"org.rdk.RDKShell.1.launch\", \"params\":{ \"callsign\": \"WebKitBrowser\", \"uri\":\"$1\" } }"
 else
-  curl -X PUT http://127.0.0.1/Service/Controller/Activate/WebKitBrowser
-  curl -d "{\"url\":\"$1\"}" http://127.0.0.1/Service/WebKitBrowser/URL
+  curl -X PUT http://127.0.0.1:9998/Service/Controller/Activate/WebKitBrowser
+  curl -d "{\"url\":\"$1\"}" http://127.0.0.1:9998/Service/WebKitBrowser/URL
 fi
 
 trap stop_browser SIGINT SIGTERM
