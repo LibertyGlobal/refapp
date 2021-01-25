e89ada7d2287c3fb456ba4ca5aa64a9cdee4ef6f
Naseem <mohammad.naseem@ltts.com>
2021-01-25 11:43:22 +0530

    Updated TV Channel List

--- src/screens/TVChannelScreen/index.js
+++ src/screens/TVChannelScreen/index.js
@@ -18,11 +18,15 @@
  */
 import { Lightning, Utils } from '@lightningjs/sdk'
 import theme from '@/themes/default'
+import * as player from '@/services/player/'
 import commonConstants from '@/constants/default'
 import constants from './constants'
-import ChannelsList from '@/components/ChannelsList'
+import { channelsServiceInit, getChannel } from '@/services/ChannelsService'
+import TVChannelScreenList from './components/TVChannelScreenList'
+import BaseScreen from '../BaseScreen'
+import { getActiveScreen, navigateForward, navigateBackward, navigate } from './../../lib/Router'
 
-export default class TVChannelScreen extends Lightning.Component {
+export default class TVChannelScreen extends BaseScreen {
   static _template() {
     return {
       ChannelListContainer: {
@@ -31,6 +35,16 @@ export default class TVChannelScreen extends Lightning.Component {
         h: commonConstants.screen.height,
         color: constants.CHLIST_CONTAINER_COLOR,
         alpha: 0.8
+      },
+      Lists: {
+        x: 0,
+        y: 60
+      },
+      ChannelListTitleBg: {
+        rect: true,
+        color: constants.CHLIST_CONTAINER_COLOR,
+        w: constants.CHLIST_CONTAINER_WIDTH,
+        h: constants.CHLIST_CONTAINER_BG_HEIGHT,
       },
       ChannelListTitle: {
         w: constants.CHLIST_CONTAINER_WIDTH,
@@ -49,27 +63,45 @@ export default class TVChannelScreen extends Lightning.Component {
         y: constants.CHLIST_TITLE_BT,
         color: constants.CHLIST_TITLE_BC
       },
-      List:{
-        type: ChannelsList,
-        zindex: 2
+      ChannelInfo: {
+        rect: true,
+        color: constants.CHLIST_CONTAINER_COLOR,
+        alpha: 0.9,
+        w: constants.CHLIST_INFO_WIDTH,
+        h: constants.CHLIST_INFO_HEIGHT,
+        x: constants.CHLIST_INFO_X,
+        y: constants.CHLIST_INFO_Y,
+        visible: false,
+        ImageIcon: {
+          w: constants.CHLIST_INFO_IMAGE_WIDTH,
+          h: constants.CHLIST_INFO_IMAGE_HEIGHT,
+          alpha: 0.5,
+
+        },
+        RefIdTxt: {
+          x: constants.CHLIST_INFO_REF_ID_X,
+          y: constants.CHLIST_INFO_REF_ID_Y,
+          text: {
+            fontSize: constants.CHLIST_INFO_FONTSIZE,
+          }
+        },
+        LocatorTxt: {
+          x: constants.CHLIST_INFO_LOCATER_X,
+          y: constants.CHLIST_INFO_LOCATER_Y,
+          w: constants.CHLIST_INFO_LOCATER_WIDTH,
+          text: {
+            fontSize: constants.CHLIST_INFO_FONTSIZE,
+            wordWrap: true
+          }
+        },
+
+
       }
     }
   }
 
-  _handleUp() {
-	 // TODO
-  }
-
-  _handleDown() {
-	  // TODO
-  }
-
-  _handleLeft() {
-	  // TODO
-  }
-
-  _handleRight() {
-	  // TODO
+  _getFocused() {
+    return this.activeList
   }
 
   _handleKey(key) {
@@ -81,22 +113,52 @@ export default class TVChannelScreen extends Lightning.Component {
   }
 
   _handleEnter() {
-    // TODO
-    if (selectedChannel) {
-      this.signal('channelChanged', { selectedChannel })
-    }
-  }
-  
-  _focus() {
-    // TODO
-    channelNumber = ChannelNumber.currentIndex;
+    let selectedItem = this.tag('Lists').children[0].activeItem._item
+    console.log(selectedItem)
+    this._play(selectedItem)
+    this.tag('ChannelInfo').visible = true;
+    this.tag('RefIdTxt').text = "channelId : " + selectedItem.channelId
+    this.tag('LocatorTxt').text = "locator : " + selectedItem.locator
+    this.tag("ImageIcon").patch({ src: Utils.asset(selectedItem.logo), x: constants.CHLIST_INFO_IMAGE_X, y: constants.CHLIST_INFO_IMAGE_Y });
+    let timer = setTimeout(function (ref) {
+      ref.tag('ChannelInfo').visible = false;
+      clearTimeout(timer);
+    }, 4000, this)
   }
 
-  updateView() {
-    // TODO
+
+  async _play(entry) {
+    await player.playQAM(entry)
   }
 
   async _init() {
-  
+
+    const channels = await channelsServiceInit();
+    let channelList = [];
+
+    for (let _index = 0; _index < channels.length; _index++) {
+      channels[_index].label = channels[_index].channelId
+    }
+
+
+    let obj = {
+      type: TVChannelScreenList,
+      itemSize: { w: constants.LIST_ITEM_WIDTH, h: constants.LIST_ITEM_HEIGHT },
+      label: "",
+      items: channels,
+      x: constants.LIST_X,
+      y: constants.LIST_Y
+    };
+    channelList.push(obj);
+    this._index = 0
+    this.tag('Lists').children = channelList;
+  }
+
+  get lists() {
+    return this.tag('Lists').children
+  }
+
+  get activeList() {
+    return this.lists[this._index]
   }
 }
