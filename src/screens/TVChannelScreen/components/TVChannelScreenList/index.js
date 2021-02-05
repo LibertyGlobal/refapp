503fa0a930452c22527448ebbac694c101b2346d
Mohammad Naseem Shekh <naseemshekh20@gmail.com>
2021-02-05 14:34:42 +0530

    TV channels list screens (#46)
    * first commit - TV channel list
    
    * removed bg img
    
    * removed img
    
    * Updated TV Channel List
    
    * fixed channel list sync issue with channel bar
    
    * updated "copyright" 2020 to 2021
    
    * fixed indentation
    
    Co-authored-by: Naseem <mohammad.naseem@ltts.com>

---
+++ src/screens/TVChannelScreen/components/TVChannelScreenList/index.js
@@ -0,0 +1,86 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 Liberty Global B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import TVChannelScreenItem from './TVChannelScreenItem'
+import ItemWrapper from '@/components/List/ItemWrapper'
+import List from '@/components/List'
+import constants from '../../constants'
+
+export default class TVChannelScreenList extends List {
+  static _template() {
+    return {
+      Label: {
+        text: {
+          fontSize: constants.LIST_LABEL_FONTSIZE
+        }
+      },
+      Items: {
+        y: 0
+      }
+    }
+  }
+
+  set items(value) {
+    this._itemsData = value
+    this._itemSize = this._itemSize ? this._itemSize : { w: constants.LIST_ITEM_DEFAULT_WIDTH, h: constants.LIST_ITEM_DEFAULT_HEIGHT }
+    this.tag('Items').children = value.map((item, index) => {
+      return {
+        type: ItemWrapper,
+        construct: TVChannelScreenItem,
+        y: index * (this._itemSize.h + constants.ITEMS_CAP),
+        size: this._itemSize,
+        item: item
+      }
+    })
+  }
+
+  get items() {
+    return this.tag('Items').children
+  }
+
+  _handleUp() {
+    if (this._index > 0) {
+      this.setIndex(this._index - 1)
+    }
+  }
+
+  _handleDown() {
+    if (this._index < this.items.length - 1) {
+      this.setIndex(this._index + 1)
+    }
+  }
+
+  setIndex(index) {
+    const prevIndex = this._index
+    this._index = index;
+    let itemCap = this._itemSize.h + constants.ITEMS_CAP;
+    let displayArea = constants.LIST_VIEW_HEIGHT;
+    let bottamFocusItem = Math.floor(displayArea / itemCap);
+
+    if (index > prevIndex) {
+      if (((index * itemCap) + this.tag('Items').y) > displayArea) {
+        this.tag('Items').setSmooth('y', (itemCap - ((index - bottamFocusItem) * itemCap)));
+      }
+    } else if (index < prevIndex) {
+      if (((index * itemCap) + this.tag('Items').y) <= 0) {
+        this.tag('Items').setSmooth('y', (itemCap - ((index + 1) * itemCap)));
+      }
+    }
+  }
+}
---
+++ src/screens/TVChannelScreen/constants.js
@@ -0,0 +1,58 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 Liberty Global B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module.exports = {
+    CHLIST_CONTAINER_WIDTH: 600,
+    CHLIST_CONTAINER_COLOR: 0xFF000000,
+    CHLIST_TITLE: "ALL CHANNELS LIST",
+    CHLIST_TITLE_FONTSIZE: 36,
+    CHLIST_TITLE_HEIGHT: 50,
+    CHLIST_TITLE_TOP: 35,
+    CHLIST_TITLE_BH: 5,
+    CHLIST_TITLE_BT: 100,
+    CHLIST_TITLE_BC: 0xFF0CB2DD,
+    CHLIST_CONTAINER_BG_HEIGHT: 100,
+    ITEM_SHADE_ALPHA: 0.5,
+    ITEM_TITLE_MOUNT_X: 0.5,
+    ITEM_TITLE_MAXLINES: 1,
+    ITEM_TITLE_FONTSIZE: 24,
+    LIST_LABEL_FONTSIZE: 30,
+    LIST_X: 70,
+    LIST_Y: 50,
+    LIST_ITEMS_Y: 60,
+    LIST_ITEM_DEFAULT_WIDTH: 300,
+    LIST_ITEM_DEFAULT_HEIGHT: 170,
+    LIST_ITEM_WIDTH: 400,
+    LIST_ITEM_HEIGHT: 50,
+    LIST_VIEW_HEIGHT: 810,
+    ITEMS_CAP: 10,
+    CHLIST_INFO_FONTSIZE: 25,
+    CHLIST_INFO_WIDTH: 1100,
+    CHLIST_INFO_HEIGHT: 150,
+    CHLIST_INFO_X: 700,
+    CHLIST_INFO_Y: 800,
+    CHLIST_INFO_IMAGE_X: 900,
+    CHLIST_INFO_IMAGE_Y: 20,
+    CHLIST_INFO_IMAGE_WIDTH: 100,
+    CHLIST_INFO_IMAGE_HEIGHT: 100,
+    CHLIST_INFO_REF_ID_X: 20,
+    CHLIST_INFO_REF_ID_Y: 10,
+    CHLIST_INFO_LOCATER_X: 20,
+    CHLIST_INFO_LOCATER_Y: 40,
+    CHLIST_INFO_LOCATER_WIDTH: 900
+}
